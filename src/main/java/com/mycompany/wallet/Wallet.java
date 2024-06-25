291f9d2f2e24d3256169a25ba65cdf16523a6003
KarenSandova <sandovalk061@gmail.com>
2024-06-25 00:18:42 -0500

    Add files via upload

---
+++ src/main/java/com/mycompany/wallet/Wallet.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wallet;
+
+/**
+ *
+ * @author karen
+ */
+public class Wallet {
+
+     public static void main(String[] args) {
+      frmTabla01 object = new frmTabla01();
+          object.setVisible(true);
+      
+    }
+}
